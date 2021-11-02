3488298a73d2af404881b07b7b81cc2d1c23350f
nhamhongphuc <Nhamphuc414@gmail.com>
2021-11-02 20:19:37 +0700

    up model
    up model review

---
+++ Project/BackEnd/BackEnd/Models/Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public enum Ranking 
+    { 
+        bad, normal, excellent
+    }
+    public class Review
+    {
+        public int ProductID { get; set; }
+        public string AccountID { get; set; }
+        public Ranking Ranking { get; set; }
+        public string Comment { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public Account Account { get; set; }
+        public Product Product { get; set; }
+    }
+}
